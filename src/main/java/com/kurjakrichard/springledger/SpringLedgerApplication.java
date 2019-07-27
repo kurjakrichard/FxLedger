35de4fda0f66b6cc2b5b1e19b990f7e928589c8b
Kurják Richárd <kurjak.richard@gmail.com>
2019-07-27 17:39:10 +0200

    Set Spring pomm.xml

---
+++ src/main/java/com/kurjakrichard/springledger/SpringLedgerApplication.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kurjakrichard.springledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author balza
+ */
+@SpringBootApplication
+public class SpringLedgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLedgerApplication.class, args);
+    }
+}
